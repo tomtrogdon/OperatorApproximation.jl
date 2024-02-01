ab49e0c893125d43c172bb3f2755daee0e33a885
Tom Trogdon <trogdon@uw.edu>
2024-01-31 20:45:58 -0800

    small update

--- src/Domain.jl
+++ src/Domain.jl
@@ -5,6 +5,7 @@ abstract type GridDomain <: Domain end
 M = (A,B,x) -> (B - A)/2*x .+ (B + A)/2  # from I to [A,B]
 iM = (A,B,x) -> 2/(B - A)*(x .- (B + A)/2) # From [A,B] to I
 
+#TODO:  Define Interval <: Domain
 struct UnitInterval <: Domain
     map::Function
     imap::Function
