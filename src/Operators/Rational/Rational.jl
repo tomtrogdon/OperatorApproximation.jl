5a25dbd631c6034540965d6075395df256997093
klilly50 <128432721+klilly50@users.noreply.github.com>
2024-05-22 10:04:53 -0700

    Update Rational.jl

--- src/Operators/Rational/Rational.jl
+++ src/Operators/Rational/Rational.jl
@@ -1,36 +1,4 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "id": "fd63d8b7",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "include(\"Conversion.jl\")\n",
-    "include(\"Derivative.jl\")\n",
-    "include(\"Multiplication.jl\")\n",
-    "include(\"Cauchy.jl\")"
-   ]
-  }
- ],
- "metadata": {
-  "@webio": {
-   "lastCommId": null,
-   "lastKernelId": null
-  },
-  "kernelspec": {
-   "display_name": "Julia 1.8.5",
-   "language": "julia",
-   "name": "julia-1.8"
-  },
-  "language_info": {
-   "file_extension": ".jl",
-   "mimetype": "application/julia",
-   "name": "julia",
-   "version": "1.8.5"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+include("Conversion.jl")
+include("Derivative.jl")
+include("Multiplication.jl")
+include("Cauchy.jl")
