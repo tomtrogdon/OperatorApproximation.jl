061513653289742f9d964846fba1003e4e71bb7a
klilly50 <128432721+klilly50@users.noreply.github.com>
2024-05-22 09:54:48 -0700

    Added Rational.jl and Cauchy.jl

---
+++ src/Bases/Rational.jl
@@ -0,0 +1,36 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "fd63d8b7",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "include(\"Conversion.jl\")\n",
+    "include(\"Derivative.jl\")\n",
+    "include(\"Multiplication.jl\")\n",
+    "include(\"Cauchy.jl\")"
+   ]
+  }
+ ],
+ "metadata": {
+  "@webio": {
+   "lastCommId": null,
+   "lastKernelId": null
+  },
+  "kernelspec": {
+   "display_name": "Julia 1.8.5",
+   "language": "julia",
+   "name": "julia-1.8"
+  },
+  "language_info": {
+   "file_extension": ".jl",
+   "mimetype": "application/julia",
+   "name": "julia",
+   "version": "1.8.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
