98ed67443bdd9cf124da59e85878a36cc232d8c7
klilly50 <128432721+klilly50@users.noreply.github.com>
2024-05-29 16:27:46 -0700

    Update Derivative.jl
    Changed function of row and column to return updated elements of derivative matrix for rational expansion derivative without the minus 1 and then built the corresponding banded matrix with that function.

--- src/Operators/Rational/Derivative.jl
+++ src/Operators/Rational/Derivative.jl
@@ -1,29 +1,5 @@
 function DerivMatrix(i,j,α)
-    i = i-1
-    j = j-1
-    if j == 0
-        if (i == 0)
-            return 1im*α
-        else
-            return 0im
-        end
-    elseif j == 1
-        if (i == 0) || (i == 2)
-            return -3*1im*j
-        elseif i == 1
-            return 1im*α
-        else
-            return -2*1im*j
-        end
-    elseif j == 2
-        if (i == 1) || (i == 3)
-            return 0im
-        elseif i == 2
-            return (3*1im*j)+(1im*α)
-        else
-            return 1im*j
-        end
-    elseif (i == (j - 1)) || (i == (j + 1))
+    if (i == (j - 1)) || (i == (j + 1))
         return -1im*j
     elseif (i == j)
         return 1im*((2*j)+α)
@@ -36,8 +12,7 @@ end
 function *(D::Derivative,domain::Rational)
     if D.order == 1
         range = domain
-#         dom = domain.GD.D
-        ConcreteOperator(domain,range,BasicMatrixOperator{ℤ,ℤ}(DerivMatrix(i,j,α)))
+        ConcreteOperator(domain,range,BasicBandedOperator{ℤ,ℤ}(1,1,DerivMatrix(i,j,α)))
     else
         Derivative(D.order-1)*(Derivative(1)*domain)
     end
