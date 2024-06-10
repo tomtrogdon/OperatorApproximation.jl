cea4cd15223a0dd44171ae656842e8ad23db344d
klilly50 <128432721+klilly50@users.noreply.github.com>
2024-06-10 11:42:22 -0700

    Update Derivative.jl
    Changed how Derivative Operator is being built so that it's correct.

--- src/Operators/OscRational/Derivative.jl
+++ src/Operators/OscRational/Derivative.jl
@@ -1,8 +1,9 @@
-function DerivMatrix(i,j,α)
+function DerivMatrix(i,j)
+    α = 2; #WE NEED TO FIND A WORKAROUND TO HARD CODING THIS!!!!!!!!!
     if (i == (j - 1)) || (i == (j + 1))
-        return -1im*j
+        return -1im*j/2
     elseif (i == j)
-        return 1im*((2*j)+α)
+        return 1im*((j)+α)
     else 
         return 0im
     end
@@ -12,7 +13,7 @@ end
 function *(D::Derivative,domain::OscRational)
     if D.order == 1
         range = domain
-        ConcreteOperator(domain,range,BasicBandedOperator{ℤ,ℤ}(1,1,DerivMatrix(i,j,α)))
+        ConcreteOperator(domain,range,BasicBandedOperator{ℤ,ℤ}(1,1,DerivMatrix))
     else
         Derivative(D.order-1)*(Derivative(1)*domain)
     end
