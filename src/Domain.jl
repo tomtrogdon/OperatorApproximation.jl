ef599c2871e80628f7f5e6910be1964beb38c744
klilly50 <128432721+klilly50@users.noreply.github.com>
2024-06-10 11:39:00 -0700

    Update Domain.jl
    Made operations in RationalRealAxis scalar

--- src/Domain.jl
+++ src/Domain.jl
@@ -304,9 +304,9 @@ struct RationalRealAxis <: GridAxis
     D::Axis
     grid::Function
     function RationalRealAxis()
-        Tm1 = z-> (1/1im)*((z+1)/(z-1)) #inverse mobius transform that maps unit circle onto real axis
-        rat_mgrid = n-> (((0:n-1)+(1/2))/n)*(2*π) #[0,2π) shifted by 1/2 to avoid issues at 0 and infinity
-        gridfun = n-> Tm1(exp(1im.*rat_mgrid(n))) #x=T^{-1}(exp(iθ))
+        Tm1 = z-> (1/1im)*((z.+1)./(z.-1)) #inverse mobius transform that maps unit circle onto real axis
+        rat_mgrid = n-> (((0:n-1).+(1/2))./n).*(2*π) #[0,2π) shifted by 1/2 to avoid issues at 0 and infinity
+        gridfun = n-> Tm1(exp.(1im.*rat_mgrid(n))) #x=T^{-1}(exp(iθ))
         return new(RealAxis(),gridfun)
     end
 end
