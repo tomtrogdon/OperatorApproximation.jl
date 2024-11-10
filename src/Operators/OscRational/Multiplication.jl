4425338de4dc63beaeb6a77ccd778b8850685913
klilly50 <128432721+klilly50@users.noreply.github.com>
2024-11-10 09:19:43 -0800

    Resolved issue with last test not passing

--- src/Operators/OscRational/Multiplication.jl
+++ src/Operators/OscRational/Multiplication.jl
@@ -7,7 +7,7 @@ function *(M::Multiplication,sp::OscRational)
         ff = M.f
         α = ff.basis.α
     end
-    
+
     if typeof(ff.basis) <: OscRational && isconvertible(ff.basis,sp)
         np = N₋(length(ff.c)); nm = length(ff.c) - np + 1 #why even define nm??
         Op = BasicBandedOperator{ℤ,ℤ}(np,np,toeplitz_function(ff.c)) #creates Toeplitz operator
@@ -15,6 +15,10 @@ function *(M::Multiplication,sp::OscRational)
         1 + 1 #TODO: just evaluate and expand, need transform #I am assumping this will just use toeplitz()
     end
 
-    sp2 = OscRational(RationalRealAxis(),α + sp.α)
-    ConcreteOperator(sp,sp2,Op) #operator in practice for multiplication
+    if α == sp.α
+        return ConcreteOperator(sp,sp,Op)
+    else
+        sp2 = OscRational(RationalRealAxis(),α + sp.α)
+        return ConcreteOperator(sp,sp2,Op) #operator in practice for multiplication
+    end
 end
