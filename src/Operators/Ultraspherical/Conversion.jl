fcb55c97e983e4f6a8d721cdf5d432c7050d4f3c
Tom Trogdon <trogdon@uw.edu>
2024-11-13 18:26:34 -0800

    Fix UnionAll

--- src/Operators/Ultraspherical/Conversion.jl
+++ src/Operators/Ultraspherical/Conversion.jl
@@ -14,7 +14,7 @@ end
 ### NOTE:  Need to get it to work and then find a way
 ### to separate matrix generation and matrix application
 #
-function fastconversion(b1::Ultraspherical,b2::GridValues{T}) where T <: Union{UltraInterval,UltraMappedInterval}
+function fastconversion(b1::Ultraspherical,b2::GridValues)
     λ = b2.GD.λ
     b3 = Ultraspherical(λ,b2.GD)
     if λ ≈ 0.0 && -1 > 0
@@ -33,7 +33,7 @@ function fastconversion(b1::Ultraspherical,b2::GridValues{T}) where T <: Union{U
     end
 end
 
-function conversion(b1::Ultraspherical,b2::GridValues{T}) where T
+function conversion(b1::Ultraspherical,b2::GridValues)
     basegrid =  n -> b2.GD.grid(n)
     # In principle, we need to do this:
     # gridfun = n -> b1.GD.D.imap(b2.GD.D.map(basegrid(n)))
