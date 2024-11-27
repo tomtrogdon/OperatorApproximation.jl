01964bdea6d4a3fffed534f91b588e36b9fe2898
Tom Trogdon <trogdon@uw.edu>
2024-11-27 10:43:12 -0800

    Added domain test for Ultraspherical

--- src/Bases/Ultraspherical.jl
+++ src/Bases/Ultraspherical.jl
@@ -41,8 +41,12 @@ function (P::BasisExpansion{Ultraspherical})(X::Number) # Clenshaw's algorithm
     n = P.c |> length
     λ = P.basis.λ
     x = P.basis.GD.D.imap(X)
-    a,b = Jacobi_ab(λ - 1/2, λ - 1/2)
-    (hcat(e(1,n) |> sparse,(jacobi(a,b,n) - x*I)[1:end-1,1:end-2] |> sparse)\P.c)[1]
+    if abs(imag(x)) > 1e-15 || abs(x) > 1
+        return 0.0
+    else
+        a,b = Jacobi_ab(λ - 1/2, λ - 1/2)
+        return (hcat(e(1,n) |> sparse,(jacobi(a,b,n) - x*I)[1:end-1,1:end-2] |> sparse)\P.c)[1]
+    end
 end
 
 function ^(f::BasisExpansion{T},x::Number) where T <: Ultraspherical
--- src/OperatorApproximation.jl
+++ src/OperatorApproximation.jl
@@ -4,7 +4,7 @@ using SparseArrays, LinearAlgebra, Plots, FFTW, AbstractFFTs, HypergeometricFunc
 import Plots: plot, plot!
 import Base: +, -, *, \, complex, /, length, iterate, log, sqrt, ==, ^,
     getindex, setindex!, firstindex, lastindex, show, getindex, size, axes,
-    real, imag, abs, sum, zero, intersect, conj, chop
+    real, imag, abs, sum, zero, intersect, conj, chop, copy
 import LinearAlgebra: I, Matrix, norm, eigen, diagm, transpose, dot
 
 export Domain, GridDomain, Basis, Derivative, Evaluation, Ultraspherical, ChebyshevInterval,
