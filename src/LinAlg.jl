9a3aefec73e4f661e8b0b6e686adf26c748380af
klilly50 <128432721+klilly50@users.noreply.github.com>
2024-11-27 16:54:52 -0800

    Fixing simp() issues among other small things (#16)
    * Adding SumDot and more tests
    
    Added SumDot capability to LinAlg.jl and added more tests to runtests.jl to make it up to date with all of the tests that I've written
    
    * Update OperatorApproximation.jl
    
    Resolving merge conflict
    
    * Changing SumDot to sumdot
    
    Fuss maintenance
    
    * Update LinAlg.jl
    
    I missed a SumDot
    
    * added chop(), combine(), and simp() functionalities for DirectSum and vectors of BasisExpansions; added \alpha check to determine if two OscRational bases are convertible
    
    * Modified Multiplication.jl to comply with new isconvertible() for OscRational
    
    * Fixing length(f) issue
    
    * changed some chops to Base.chop()
    
    * Modifications to simp(), sumdot(), and Multiplication.jl
    
    * additional testing file
    
    * LinAlg got weird in the merge
    
    * slight error with what I kept in LinAlg.jl
    
    * getting rid of stray test file

--- src/LinAlg.jl
+++ src/LinAlg.jl
@@ -63,6 +63,10 @@ function dot(f::BasisExpansion{T},g::BasisExpansion{T}) where T <: DirectSum
     return inner_prod
 end
 
+function sumdot(f::BasisExpansion,g::BasisExpansion)
+    return dot(f,g)
+end
+
 #if f=[a,b], g=[c,d], this does <a,c> + <a,d> + <b,c> + <b,d>
 function sumdot(f::BasisExpansion{T},g::BasisExpansion{T}) where T <: DirectSum
     inner_prod = 0
@@ -90,7 +94,7 @@ function sumdot(f::BasisExpansion{T},g::BasisExpansion) where T <: DirectSum
     return inner_prod
 end
 
-function sumdot(v1::Vector{T},v2::Vector{T}) where T <: BasisExpansion
+function sumdot(v1::Vector,v2::Vector)
     inner_prod = 0
     for j=1:length(v2)
         inner_prod += sumdot(v1[j],v2[j])
